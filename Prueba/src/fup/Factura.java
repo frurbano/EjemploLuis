34370798670e3d1795d74b8be1a4cd9f6dbfa9c4
Franco Urbano <frurbano5@gmail.com>
2023-01-27 17:13:28 -0500

    Se agrega clase Factura

---
+++ Prueba/src/fup/Factura.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fup;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Factura {
+    int edad=10;
+}
